a73604c984e041efa40c593602f8bffe19e030ad
Peter Etelej <peter@etelej.com>
2017-06-03 15:37:24 +0300

    Use single instance propert for active form

--- web/js/main.js
+++ web/js/main.js
@@ -1,28 +1,30 @@
 var app = new Vue({
 	el: "#app",
 	data: {
-		activeform: "",
+		activeform: "bitmap",
 		value:"",
 		result:"",
 	},
 	computed: {
-		form: function(){
-			if (this.activeform===""){
-				return 'bitmap'
-			}
-			return this.activeform
-		},
 		placeholder:function(){
 			switch(this.activeform){
 				case "bitmap":
-				case "":
 					return "Enter Bitmap"
 			}
+		},
+		formtitle: function(){
+			switch(this.activeform){
+				case "bitmap":
+					return "Convert bitmap to binary string"
+			}
 		}
-
 	},
 	methods: {
 		bitmapToBinary: function(){
+			if (this.value==""){
+				newResult("Error: Enter a value into the form for conversion.")
+				return
+			}
 			newResult("(fetching result...)")
 			axios.get("/api/bitmaptobin",{
 				params:{
@@ -35,7 +37,7 @@ var app = new Vue({
 				.catch(function(error){
 					console.log(error)
 					if (error.data===undefined){
-						newResult("Oops! something went wrong. Confirm the value you submitted, must be hex.")
+						newResult("Oops! something went wrong. Confirm the value you submitted, must be hex bitmap.")
 						return
 					}
 					newResult("Error: "+ error.data.response)
